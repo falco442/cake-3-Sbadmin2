0f2912e300865e74998223de9e6dce4225ab514a
falco442 <marcofra83@gmail.com>
2016-11-10 12:48:35 +0100

    Correction of button template

--- src/View/Helper/FormHelper.php
+++ src/View/Helper/FormHelper.php
@@ -8,8 +8,14 @@ use Cake\View\View;
 class FormHelper extends OldHelper{
 
 	protected $_defaultFormOptions = [
-		'class'=>'form-control',
-		'div'=>'form-group'
+		'text'=>[
+			'class'=>'form-control',
+			'div'=>'form-group'
+		],
+		'button'=>[
+			'class'=>'btn btn-default',
+			'div'=>'form-group'
+		]
 	];
 
 	public function initialize(array $config){
@@ -17,7 +23,7 @@ class FormHelper extends OldHelper{
 
 		// se ../config/app_form.php
 		$this->config('templates',[
-	        'button' => '<button class="btn" {{attrs}}>{{text}}</button>',
+	        // 'button' => '<button class="btn" {{attrs}}>{{text}}</button>',
 	        'checkboxFormGroup' => '<div class="checkbox">{{label}}</div>',
 	        'checkboxWrapper' => '<div class="checkbox">{{label}}</div>',
 	        'file' => '<input class="form-control" type="file" name="{{name}}"{{attrs}}>',
@@ -34,12 +40,22 @@ class FormHelper extends OldHelper{
 	}
 
 	public function input( $fieldName , array $options = [] ){
-		$options = array_merge_recursive($this->_defaultFormOptions,$options);
+		if(isset($options['type']) && $options['type']=='button'){
+			$options = array_merge_recursive($this->_defaultFormOptions['button'],$options);
+			$options['class'] = 'btn btn-default '.$options['class'];
+		}else{
+			$options = array_merge_recursive($this->_defaultFormOptions['text'],$options);
+		}
 		if(isset($options['type']) && $options['type']=='date'){
 			$options['type'] = 'text';
 			$options['class'] .= ' datepicker';
 		}
 		return parent::input($fieldName,$options);
+	}
+
+	public function button( $title , array $options = [] ){
+		$options = array_merge_recursive($this->_defaultFormOptions['button'],$options);
+		return parent::button($title,$options);
 	}
 
 	public function __construct(View $View, array $config = []){
